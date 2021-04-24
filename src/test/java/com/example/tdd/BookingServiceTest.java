7ddd5c01ddac979fc34344a0fe6168b6d6dcec9c
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-04-24 00:05:15 -0300

    create BookingServiceTestConfiguration

--- src/test/java/com/example/tdd/BookingServiceTest.java
+++ src/test/java/com/example/tdd/BookingServiceTest.java
@@ -1,17 +1,28 @@
 package com.example.tdd;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import com.example.tdd.service.BookingService;
 
-@SpringBootTest
+
 @ExtendWith(SpringExtension.class)
 public class BookingServiceTest {
-
+	
+	@TestConfiguration
+	static class BookingServiceTestConfiguration{
+		
+		@Bean
+		public BookingService bookingService() {
+			return new BookingService();
+		}
+	}
+	
 	@Autowired
 	BookingService bookingService;
 	
