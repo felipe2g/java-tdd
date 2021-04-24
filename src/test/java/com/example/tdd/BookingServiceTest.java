6937fa2b1ae936fbf0fd51e5b50e960cc6243961
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-04-23 23:45:00 -0300

    pass on test bookingTestServiceDaysCalculator

--- src/test/java/com/example/tdd/BookingServiceTest.java
+++ src/test/java/com/example/tdd/BookingServiceTest.java
@@ -1,17 +1,15 @@
 package com.example.tdd;
 
-
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
+import com.example.tdd.service.BookingService;
 
+@SpringBootTest
 @ExtendWith(SpringExtension.class)
-
 public class BookingServiceTest {
 
 	@Autowired
